2385b691c2df36dfefe5eb304a1aa8d55377ca38
Michael Wachell <michaelwachell@Michaels-Mac-Pro.local>
2018-07-10 15:48:59 -0700

    make super chill

--- src/helpers.js
+++ src/helpers.js
@@ -13,15 +13,25 @@ const clearBoard = () => {
 };
 
 // clear the board, populate the game object with players
-const startGame = () => {
-  let form = document.querySelectorAll(".player-form");
+const startGame = (player1, player2) => {
   let p1 = document.getElementById("p1");
   let p2 = document.getElementById("p2");
+  game = new TicGame();
 
   clearBoard();
-  game.p1 = p1.value || "Player 1";
-  game.p2 = p2.value || "Player 2";
+  game.p1 = p1.value || player1 || "Player 1";
+  game.p2 = p2.value || player2 || "Player 2";
+  console.log(game)
   p2.value = "";
   p1.value = "";
   game.updateMessage(`It's ${game.p1}'s turn`);
 };
+
+const snackBar = (message) => {
+
+ var x = document.getElementById("snackbar");
+ x.innerText = message;
+ x.className = "show";
+ setTimeout(function(){ x.className = x.className.replace("show", ""); }, 3000);
+
+}
